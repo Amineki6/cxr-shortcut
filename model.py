dbc7e5c2d2822c473dd507e922b16d0fe4870716
Eike Petersen <1774207+e-pet@users.noreply.github.com>
2026-01-18 14:47:55 +0100

    fix to dino model attempt

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class CXP_Model(nn.Module):
         # 1. Load the Backbone (DenseNet121)
         #self.encoder = densenet121(weights='IMAGENET1K_V1')
         model_size = 'tiny'
-        self.encoder = torch.hub.load('facebookresearch/dinov3', f'dinov3_convnext_{model_size}',
+        self.encoder = torch.hub.load('../dinov3', f'dinov3_convnext_{model_size}',
                                 weights=f"/dino/dinov3_convnext_{model_size}_pretrain_lvd1689m-21b726bb.pth")   
         
         # Get the feature dimension (1024 for DenseNet121)
